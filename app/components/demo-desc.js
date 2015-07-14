c9c6d19c1765254dd1ed313af8add2e813765568
lolmaus (Andrey Mikhaylov) <lolmaus@gmail.com>
2015-07-14 13:01:18 +0300

    Scroll to top on switching demo/desc

--- app/components/demo-desc.js
+++ app/components/demo-desc.js
@@ -12,6 +12,12 @@ export default Ember.Component.extend({
   click () {
     //if ($(evt.target).closest('.checkBox').length) return;
     this.toggleProperty('modeIsDemo');
+    this.scrollToTop();
+  },
+
+  // CUSTOM METHODS
+  scrollToTop () {
+    window.scrollTo(0, 0);
   },
 
   // ACTIONS
@@ -19,18 +25,22 @@ export default Ember.Component.extend({
     pan (val) {
       if (val === "up" && this.$().width() <= 50) {
         this.set('modeIsDemo', true);
+        this.scrollToTop();
       }
 
       if (val === "down" && this.$().width() <= 50) {
         this.set('modeIsDemo', false);
+        this.scrollToTop();
       }
 
       if (val === "left" && this.$().width() > 50) {
         this.set('modeIsDemo', false);
+        this.scrollToTop();
       }
 
       if (val === "right" && this.$().width() > 50) {
         this.set('modeIsDemo', true);
+        this.scrollToTop();
       }
     }
   }
